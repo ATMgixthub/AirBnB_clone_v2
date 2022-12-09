d2939c411e2000fcd1328e52f198f78e4bd715eb
ATMgixthub <105108630+ATMgixthub@users.noreply.github.com>
2022-12-09 12:29:27 +0000

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-"""
-Module Review class
-"""
-from models.base_model import BaseModel
+'''
+    Implementation of the Review class
+'''
+from os import getenv
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
-class Review(BaseModel):
-    """
-    Inherits from BaseModel
-    Public class attributes:
-        place_id:            (str) will be Place.id
-        user_id:             (str) will be User.id
-        text:                (str)
-    """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    '''
+        Implementation for the Review.
+    '''
+    __tablename__ = "reviews"
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
