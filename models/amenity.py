9da0d38fcd9a443a8bcbb349e8be150bc2d6bde2
ATMgixthub <105108630+ATMgixthub@users.noreply.github.com>
2022-12-09 12:25:29 +0000

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
-"""
-Module Amenity class
-"""
-from models.base_model import BaseModel
+'''
+    Implementation of the Amenity class
+'''
+from os import getenv
+from models.base_model import BaseModel, Base
+from models.place import place_amenity
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    """
-    Inherits from BaseModel
-    Public class attribute:
-        name: (str)
-    """
-    name = ""
+class Amenity(BaseModel, Base):
+    '''
+        Implementation for the Amenities.
+    '''
+    __tablename__ = "amenities"
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity,
+                                       back_populates="amenities")
+    else:
+        name = ""
